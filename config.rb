f9891858d607d89f909e6b7cb31363117f520d9c
井村 圭介 <keisuke@imura-no-iMac.local>
2014-02-24 15:51:04 +0900

    foundation5のsass、font-awesomeの組み込み

--- config.rb
+++ config.rb
@@ -1,5 +1,5 @@
 # Require any additional compass plugins here.
-add_import_path "lib/bower-foundation/scss"
+add_import_path "lib/foundation5-sass/scss"
 
 # Set this to the root of your project when deployed:
 http_path = "./"
