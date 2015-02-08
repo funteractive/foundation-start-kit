6d2b1bdc755af90647ac6795d9492db3d41b2018
Keisuke Imura <keisuke@keisuke-imura.com>
2015-02-08 10:36:39 +0900

    gulpfile作成まで

---
+++ gulpfile.js
@@ -0,0 +1,100 @@
+'use strict';
+
+// Funteractive start up theme
+//
+// We use this Gulpfile to assemble the documentation, run unit tests,
+// and deploy changes to the live documentation and CDN.
+//
+// The tasks are grouped into these categories:
+//   1. Libraries
+//   2. Variables
+//   3. Cleaning files
+//   4. Copying files
+//   5. Stylesheets
+//   6. JavaScript
+//   7. Testing
+//   8. Server
+//   9. Deployment
+//  10. Default tasks
+
+// 1. LIBRARIES
+// - - - - - - - - - - - - - - -
+
+var gulp           = require('gulp'),
+  $              = require('gulp-load-plugins')(),
+  rimraf         = require('rimraf'),
+  runSequence    = require('run-sequence'),
+  modRewrite     = require('connect-modrewrite'),
+  routes         = require('./bin/gulp-dynamic-routing'),
+  merge          = require('merge-stream'),
+  settingsParser = require('settings-parser')
+  ;
+
+
+// BUILD
+// - - - - - - - - - - - - - - -
+
+// Generate Sass settings file
+gulp.task('settings', function() {
+  return settingsParser([
+    'scss/_includes.scss',
+    'scss/_global.scss',
+    'scss/helpers/_breakpoints.scss',
+    'scss/components/_typography.scss',
+    'scss/components/_grid.scss',
+    'scss/components/*.scss'
+  ], {
+    title: 'Foundation for Apps Settings'.toUpperCase(),
+    partialsPath: 'build/partials/scss',
+    settingsPath: 'scss'
+  });
+});
+
+
+
+// JADE
+// - - - - - - - - - - - - - - -
+
+
+// STYLESHEET
+// - - - - - - - - - - - - - - -
+// Compile stylesheets with Ruby Sass
+gulp.task('sass', function() {
+  var filter = $.filter(['*.css']);
+
+  return $.rubySass('docs/assets/scss/', {
+    loadPath: ['scss'],
+    style: 'nested',
+    bundleExec: true
+  })
+    .on('error', function(err) {
+      console.log(err.message);
+    })
+    .pipe(filter)
+    .pipe($.autoprefixer({
+      browsers: ['last 2 versions', 'ie 10']
+    }))
+    .pipe(filter.restore())
+    .pipe(gulp.dest('./build/assets/css/'));
+});
+
+
+
+// STYLE GUIDE
+// - - - - - - - - - - - - - - -
+
+
+// IMAGE
+// - - - - - - - - - - - - - - -
+
+
+// JAVASCRIPT
+// - - - - - - - - - - - - - - -
+
+
+// WATCH
+// - - - - - - - - - - - - - - -
+
+
+// PRODUCTION
+// - - - - - - - - - - - - - - -
